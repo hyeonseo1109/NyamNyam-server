09a917d0a8e66567d0be1318ee75b0145834d879
hyeonseo1109 <alsgustj2@naver.com>
2025-08-29 23:45:47 +0900

    feat: Places 찜기능 코드 추가

---
+++ routes/api/places.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { pool } from "../db.js";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { userId, restaurantId } = req.body;
+
+  try {
+    // DB에 저장
+    await pool.query(
+      "INSERT INTO Places (userId, restaurantId) VALUES (?, ?)",
+      [userId, restaurantId]
+    );
+
+    res.json({ message: "찜 : success" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "찜 : 서버 에러" });
+  }
+});
+
+router.delete("/", async (req, res) => {
+  const {userId, restaurantId} = req.body;
+  try {
+    await pool.query(
+      "DELETE FROM Places WHERE userId = ? AND restaurantId = ?",
+      [userId, restaurantId]
+    );
+    res.json({ message: "찜 취소"});
+  } catch (err) {
+    console.error("에러:", err);
+    res.status(500).json({message: "찜 취소 : 서버 에러"});
+  }
+});
+
+export default router;
